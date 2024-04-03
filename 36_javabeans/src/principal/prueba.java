e1ed579bf6398d4c57d78ef3b6c69121fde6ec32
manana <manana@host.docker.internal>
2024-04-03 13:59:50 +0200

    36

---
+++ 36_javabeans/src/principal/prueba.java
@@ -0,0 +1,14 @@
+package principal;
+
+import model.Ciudad;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		Ciudad ciudad1 = new Ciudad("prueba1" , 30000, 20.4);
+		//Array tipo Ciudad
+		ArrayList <Ciudad> ciudades = new ArrayList<>();
+
+	}
+
+}
